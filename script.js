05c8029537d935426021ae25af5d01fd3c6cd7c0
unknown <karthithetechieguy@gmail.com>
2023-02-01 08:37:29 +0530

    Random color palette implementation

---
+++ script.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".container");
+const refreshButton = document.querySelector(".refresh-btn");
+
+// generating random hex value 
+const generateRandomHexColor = () => {
+    let randomHex = Math.floor(Math.random() * 0xffffff).toString(16)
+    return `#${randomHex.padStart(6, '0')}`;
+}
+
+const maxPaletteBoxes = 15;
+const generatePalette = () => {
+    container.innerHTML = ""; // clearing the container
+    for (let i = 0; i < maxPaletteBoxes; i++) {
+        let randomHex = generateRandomHexColor();
+        // creating li element to append to container
+        const color = document.createElement('li');
+        color.classList.add('color');
+        color.innerHTML = `<div class="rect-box" style="background:${randomHex}"></div>
+        <span class="hex-value">
+            ${randomHex}
+        </span>`
+        // adding click event to current li element to copy the color
+        color.addEventListener("click", () => copyHex(color, randomHex))
+        container.appendChild(color)
+    }
+}
+
+const copyHex = (elem, hexValue) => {
+    const hexElement = elem.querySelector(".hex-value");
+    // copy the color code in clipboard and set it as copied
+    // after 1 sec, again showing the hex color value
+    navigator.clipboard.writeText(hexValue).then(() => {
+        hexElement.innerText = "Copied";
+        setTimeout(() => {
+            hexElement.innerText = hexValue;
+        }, 1000)
+    }).catch(() => {
+        // showing alert while copying color code
+        alert("Failed to copy the color code!")
+    })
+}
+
+generatePalette();
+refreshButton.addEventListener("click", generatePalette);
